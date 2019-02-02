9c37724bb0bd97f44bfb2232fce60e553d54c480
almog <almogsb@gmail.com>
2019-02-02 12:27:17 +0200

    first commit - for check

--- Crawler.py
+++ Crawler.py
@@ -6,7 +6,6 @@ from bs4 import BeautifulSoup
 
 #headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'} # This is chrome, you can set whatever browser you like
 #response = requests.get('https://coinmarketcap.com/', headers=headers)
-#print(response.status_code)
 
 my_session = requests.session()
 for_cookies = my_session.get("https://coinmarketcap.com/")
